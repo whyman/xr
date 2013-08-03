21985d003dade86042982ee44502eddacfec7cf9
Ian Whyman <ian.whyman@sabayon.org>
2013-08-03 15:09:41 +0100

    Add License info (GPLv3)

--- src/net/v00d00/xr/HomeActivity.java
+++ src/net/v00d00/xr/HomeActivity.java
@@ -1,3 +1,23 @@
+/*
+
+XR - Modern Android XBMC remote
+Copyright (C) 2013 Ian Whyman
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package net.v00d00.xr;
 
 import java.io.File;
@@ -11,19 +31,14 @@ import org.xbmc.android.jsonrpc.api.model.AudioModel.AlbumDetail;
 import org.xbmc.android.jsonrpc.io.ConnectionManager;
 
 import android.app.ActionBar;
-import android.content.Context;
 import android.net.http.HttpResponseCache;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
